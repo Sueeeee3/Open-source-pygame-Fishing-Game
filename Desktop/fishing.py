b4da3b59e43d7b174dfeea6e73680f0d21d541ff
Sue <zuzanna.mystkowska123@gmail.com>
2026-03-04 08:44:23 +0100

    commenting
    Refactor FishingSystem class for improved clarity and maintainability.

--- Desktop/fishing.py
+++ Desktop/fishing.py
@@ -10,62 +10,64 @@ COLOR_MAP = {"default": "n", "black": "b", "pink": "p"} #Map rod color variants 
 
 class FishingSystem:
     def __init__(self, shop, game):
-        #Fishing system will get called with 2 parameters: game and shop , to not have the 
+        #Fishing system will get called with 2 parameters: game and shop (Game and Shop class), calling it this way makes sure that there wont be double imported
         self.shop = shop
         self.game = game
-        self.assets = game.assets
+        self.assets = game.assets #Use assets , take the dependency from Game class 
 
-        self.tier_top_bounds = {1: 533, 2: 333, 3: 100}
+        self.tier_top_bounds = {1: 533, 2: 333, 3: 100} #Adding tier bounds as arrays so it can be eaisly used later
 
-        self.rod_handle_offsets = [
-            pygame.Vector2(S(130), S(370)),
-            pygame.Vector2(S(129), S(371)),
-            pygame.Vector2(S(129), S(371)),
-            pygame.Vector2(S(171), S(371)),
-            pygame.Vector2(S(170), S(370)),
+        #Making a vector offset from rod images (since the rod right and rod half right are flipped versions of left and half left the without this the rod would be uneven on the screen.
+        self.rod_handle_offsets = [ 
+            pygame.Vector2(130, 370),
+            pygame.Vector2(129, 371),
+            pygame.Vector2(129, 371),
+            pygame.Vector2(171, 371),
+            pygame.Vector2(170, 370),
         ]
-
+        #The same but just with rod tops, used in Rope() class to calculate starting rope point 
         self.rod_top_offsets = [
-            pygame.Vector2(-S(127), -S(366)),
-            pygame.Vector2(-S(81),  -S(371)),
-            pygame.Vector2(0,       -S(371)),
-            pygame.Vector2(S(81),   -S(371)),
-            pygame.Vector2(S(127),  -S(366)),
+            pygame.Vector2(127, 366),
+            pygame.Vector2(81,  (371),
+            pygame.Vector2(0,       -371),
+            pygame.Vector2(81,   -371),
+            pygame.Vector2(127,  -366),
         ]
 
-        self.rod_pos = pygame.Vector2(S(960), HEIGHT)
-        self._zone_index = 2
-        self.rod_top = self.rod_pos + self.rod_top_offsets[2]
+        self.rod_pos = pygame.Vector2(960, HEIGHT) #Starting rod position, althought it wont change mich later.
+        self._zone_index = 2 #Calculating current zone (0-4) used to decide what rod image would be used; Start with zone 2 (middle)
+        self.rod_top = self.rod_pos + self.rod_top_offsets[2] #Calculating rod top from ealier offset and positon
 
-        self.bait_pos = pygame.Vector2(S(960), HEIGHT // 2)
-        self.bait_vel = pygame.Vector2(0, 0)
+        self.bait_pos = pygame.Vector2(960, HEIGHT // 2) #Starting bait positon, higher than rod but on the same x-axis
+        self.bait_vel = pygame.Vector2(0, 0) #Empty Bait velocity vector , used for later calcualtions
+        #Hardcoded physics values
         self.spring  = 0.1
         self.damping = 0.3
         self.speed   = 0.0
 
-        self.bait_img = self._get_bait_img()
-        self.scaler = Scale(self.bait_img)
-        self._build_rod_states()
-        self.rod_img = self.rod_states[2]
+        self.bait_img = self._get_bait_img() #Using _get_bait to get current bait image (line 60)
+        self.scaler = Scale(self.bait_img) #Scaling bait image using the Scale class
+        self._build_rod_states() #Calling function (line 56)
+        self.rod_img = self.rod_states[2] # based on rod state (0-4) call the corresponding image
 
-    def _color_key(self):
-        return COLOR_MAP.get(self.shop.rod_cosmetic.get(self.shop.rod_tier, "default"), "n")
+    def _color_key(self):  #Getting the rod variant from shop, setting the defalut one to be well.. normal
+        return COLOR_MAP.get(self.shop.rod_cosmetic.get(self.shop.rod_tier, "default"), "n") 
 
-    def _build_rod_states(self):
+    def _build_rod_states(self): #Based on tier and colour key get the 5 state images for current rod
         tier, ck = self.shop.rod_tier, self._color_key()
         self.rod_states = [self.assets.get_rod(tier, ck, o) for o in ("l", "hl", "f", "hr", "r")]
 
-    def _get_bait_img(self):
+    def _get_bait_img(self): #3rd tier of rod has custom bait based on colour, write if function calling the bait image if tier is 3 and use the currrent colour_key.
         tier = self.shop.rod_tier
         if tier == 1:
             return self.assets.bait_1
         if tier == 2:
             return self.assets.bait_2
         color = self.shop.rod_cosmetic.get(3, "default")
-        return {"default": self.assets.bait_3_n, "black": self.assets.bait_3_b, "pink": self.assets.bait_3_p}.get(color, self.assets.bait_3_n)
+        return {"default": self.assets.bait_3_n, "black": self.assets.bait_3_b, "pink": self.assets.bait_3_p}.get(color, self.assets.bait_3_n) 
 
-    def get_top_bound(self):
-        return self.tier_top_bounds.get(self.shop.rod_tier, S(533))
+    def get_top_bound(self): #Get the current rod tier and based on it check the top_bound, by defalut make it the smallest one.
+        return self.tier_top_bounds.get(self.shop.rod_tier, 533)
 
     def get_depth(self):
         return max(0.0, min(1.0, (S(bottom_bound) - self.bait_pos.y) / (S(bottom_bound) - S(top_bound))))
@@ -98,3 +100,4 @@ class FishingSystem:
         scaled_bait = self.scaler.get_scaled(scale)
         screen.blit(self.rod_img, self.rod_pos - self.rod_handle_offsets[self._zone_index])
         screen.blit(scaled_bait, scaled_bait.get_rect(center=self.bait_pos))
+
